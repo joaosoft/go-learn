f1af688a0130d0722463bbb863b94c160b11ecc1
João Ribeiro <joaosoft@gmail.com>
2017-07-06 13:07:32 +0100

    null with pointers

--- examples/59_null/null_pointer.go
+++ examples/59_null/null_pointer.go
@@ -3,23 +3,22 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"gopkg.in/guregu/null.v3"
 )
 
-type Teste struct {
-	String            null.String     `json:"string"`
-	Int               null.Int   	 `json:"int"`
+type TestePointer struct {
+	String            *string     `json:"string"`
+	Int               *int   	 `json:"int"`
 }
 
 func main() {
 	fmt.Println("\n\n\n\n\n######### TESTE 1 #########")
 
-	xstr := null.StringFrom("teste 1")
-	xint := null.IntFrom(123)
+	xstr := "teste 1"
+	xint := 123
 
-	teste := Teste {
-		String:      xstr,
-		Int:         xint,
+	teste := TestePointer {
+		String:      &xstr,
+		Int:         &xint,
 	}
 	fmt.Println("TESTE: ", teste)
 
@@ -31,47 +30,40 @@ func main() {
 	fmt.Println("BYTES: ", bytes)
 
 
-	var teste1 Teste
-	if err := json.Unmarshal([]byte(`{"string":"", "int": 3}`), &teste1); err != nil {
+	var teste1 TestePointer
+	if err := json.Unmarshal(bytes, &teste1); err != nil {
 		fmt.Println("Error parsing JSON", err)
 	}
 	fmt.Println("TESTE 1: ", teste1)
 
-	if !teste1.String.Valid {
-	   fmt.Printf("is null!!!\n")
-	}
-	fmt.Printf("\n\nmy_string: %s\n", teste1.String.String)
-	fmt.Printf("my_int: %d\n", teste1.Int.Int64)
+	fmt.Printf("\n\nmy_string: %s\n", *teste1.String)
+	fmt.Printf("my_int: %d\n", *teste1.Int)
+
+
 
 
 
 	fmt.Println("\n\n\n\n\n######### TESTE 2 #########")
-	var teste2 Teste
-	if err := json.Unmarshal([]byte(`{"string":null, "int": 3}`), &teste2); err != nil {
+
+
+	teste2 := TestePointer {
+	}
+	fmt.Println("TESTE: ", teste2)
+
+
+	bytes2, err := json.Marshal(teste2)
+	if err != nil {
+		fmt.Println("JSON Encoding Error", err)
+	}
+	fmt.Println("BYTES: ", bytes2)
+
+
+	var teste1_2 TestePointer
+	if err := json.Unmarshal(bytes2, &teste1_2); err != nil {
 		fmt.Println("Error parsing JSON", err)
 	}
-	fmt.Println("TESTE 2: ", teste2)
+	fmt.Println("TESTE 1: ", teste1_2)
 
-	if !teste2.String.Valid {
-		fmt.Printf("is null!!!\n")
-	}
-	fmt.Printf("\n\nmy_string: %s\n", teste2.String.String)
-	fmt.Printf("my_int: %d\n", teste2.Int.Int64)
-
-
-
-
-
-	fmt.Println("\n\n\n\n\n######### TESTE 3 #########")
-	var teste3 Teste
-	if err := json.Unmarshal([]byte(`{"int": 3}`), &teste3); err != nil {
-		fmt.Println("Error parsing JSON", err)
-	}
-	fmt.Println("TESTE 3: ", teste3)
-
-	if !teste3.String.Valid {
-		fmt.Printf("is null!!!\n")
-	}
-	fmt.Printf("\n\nmy_string: %s\n", teste3.String.String)
-	fmt.Printf("my_int: %d\n", teste3.Int.Int64)
+	fmt.Printf("\n\nmy_string is nil: %s\n", teste1_2.String == nil)
+	fmt.Printf("my_int is nil: %d\n", teste1_2.Int == nil)
 }
